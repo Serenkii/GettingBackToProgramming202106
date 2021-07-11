5589fb751a5a6ce0e8d53cb8f643b5fa799499bc
Marian <76261221+Serenkii@users.noreply.github.com>
2021-07-11 11:50:39 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
-    PrimeFinder primeFinder;
-    FibonacciFinder fibonacciFinder;
+    private PrimeFinder primeFinder;
+    private FibonacciFinder fibonacciFinder;
 
     public static void main(String[] args) {
         new Main();
@@ -26,10 +26,6 @@ public class Main {
         return primeFinder;
     }
 
-    public FibonacciFinder getFibonacciFinder() {
-        return fibonacciFinder;
-    }
-
     public void stopPrimeFinder() {
         if (primeFinder == null || !primeFinder.isRunning())
             return;
@@ -37,5 +33,33 @@ public class Main {
         System.out.println("The last found prime is " + primeFinder.getLatestPrime());
         //System.err.println("DEBUG: It will stop finding");
     }
+
+    public void startFibonacciFinder(boolean printFibonacciNumbers) {
+        fibonacciFinder.setPrintWhileFinding(printFibonacciNumbers);
+        if (fibonacciFinder.isRunning())
+            return;
+        fibonacciFinder.setFibonacciToPrint(-1);
+        new Thread(fibonacciFinder).start();
+    }
+
+    public void startFibonacciFinder(long fibonacciToPrint) {       //TODO: If the ArrayList in FibonacciFinder has already found the number, it won't be printed.
+        fibonacciFinder.setPrintWhileFinding(false);
+        fibonacciFinder.setFibonacciToPrint((int) fibonacciToPrint);
+        if (fibonacciFinder.isRunning())
+            return;
+        new Thread(fibonacciFinder).start();
+    }
+
+    public void stopFibonacciFinder() {
+        if (fibonacciFinder == null || !fibonacciFinder.isRunning())
+            return;
+        fibonacciFinder.stopFinding();
+        System.out.println("The last found fibonacci number is a(" + fibonacciFinder.getNumberOfFoundFibonaccis() + ") = " + fibonacciFinder.getLatestFibonacciNumber());
+    }
+
+    public FibonacciFinder getFibonacciFinder() {
+        return fibonacciFinder;
+    }
+
 
 }
